da51794320825871f459e019a21458993629caa2
kalamulmazid <kalam.ul.mazid@gmail.com>
2017-05-29 13:27:07 -0400

    Initial Commit

---
+++ src/main/java/com/processor/Text_CDR_Processor/App.java
@@ -0,0 +1,13 @@
+package com.processor.Text_CDR_Processor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
